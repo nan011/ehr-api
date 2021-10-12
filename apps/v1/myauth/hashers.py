0d28c0ad7a086bfa1632c3fccd355f4c4f125d2f
Nandhika Prayoga <nandhika.dev@gmail.com>
2021-10-12 16:11:44 +0700

    Adjust argon2 parameter names

--- apps/v1/myauth/hashers.py
+++ apps/v1/myauth/hashers.py
@@ -2,6 +2,6 @@ from django.contrib.auth.hashers import Argon2PasswordHasher
 import os
 
 class CustomArgon2PasswordHasher(Argon2PasswordHasher):
-    time_cost = os.getenv("TIME_COST", 2)
-    memory_cost = os.getenv("MEMORY_COST", 512)
-    parallelism = os.getenv("PARALLELISM", 2)
+    time_cost = os.getenv("ARGON2_TIME_COST", 2)
+    memory_cost = os.getenv("ARGON2_MEMORY_COST", 512)
+    parallelism = os.getenv("ARGON2_PARALLELISM", 2)
