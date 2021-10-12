af6325bd9a79cb3020d5d5c03d54d17c2824589b
Nandhika Prayoga <nandhika.dev@gmail.com>
2021-10-12 18:04:49 +0700

    convert str to int for argon2 parameter

--- apps/v1/myauth/hashers.py
+++ apps/v1/myauth/hashers.py
@@ -2,6 +2,6 @@ from django.contrib.auth.hashers import Argon2PasswordHasher
 import os
 
 class CustomArgon2PasswordHasher(Argon2PasswordHasher):
-    time_cost = os.getenv("ARGON2_TIME_COST", 2)
-    memory_cost = os.getenv("ARGON2_MEMORY_COST", 512)
-    parallelism = os.getenv("ARGON2_PARALLELISM", 2)
+    time_cost = int(os.getenv("ARGON2_TIME_COST", 2))
+    memory_cost = int(os.getenv("ARGON2_MEMORY_COST", 512))
+    parallelism = int(os.getenv("ARGON2_PARALLELISM", 2))
